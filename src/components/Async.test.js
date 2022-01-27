5aa1df4f635f10b5160670105a72410318c5a112
Daniel Frimu <frimudaniel17@gmail.com>
2022-01-27 17:58:02 +0100

    392

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,10 @@ import Async from "./Async";
 describe("Async component", () => {
   test("renders post if the request succeeds", async () => {
     // Arrage
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
     render(<Async />);
 
     // Assert
@@ -12,7 +16,7 @@ describe("Async component", () => {
   });
 });
 
-// difference between find and get
+// difference between find and get (ex: findAllByRole & getAllByRole)
 // find returns a promise so it will wait, the default is 1s
 
 // with async it will wait until our test is really done
